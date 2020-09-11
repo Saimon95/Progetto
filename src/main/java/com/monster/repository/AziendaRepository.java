2f57e764a009be7250a379be678552d09d10ee3d
jefersson00 <jeferssonserrano00@gmail.com>
2020-09-11 23:05:48 +0200

    created repository Azienda

--- src/main/java/com/monster/repository/AziendaRepository.java
+++ src/main/java/com/monster/repository/AziendaRepository.java
@@ -1,5 +1,14 @@
 package com.monster.repository;
 
-public interface AziendaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.monster.persistence.entity.Azienda;
+
+public interface AziendaRepository extends JpaRepository<Azienda, Long>{
+	
+	
+	Azienda findByNome(int id);
+	Azienda findByEmail(String email);
+	
+	
 }
